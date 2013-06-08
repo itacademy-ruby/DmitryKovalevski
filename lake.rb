3660a29e27627d0e4889f9807d4610f45d75c13a
Ubuntu_Ruby <user@ubuntu.(none)>
2013-06-08 12:24:01 +0300

    prud

---
+++ lake.rb
@@ -0,0 +1,48 @@
+module Flying
+  def fly; 'flying' end
+end
+
+module Quacking
+  def quack; 'quacking' end
+end
+
+module Swimming
+  def swim; 'swimming' end
+  def self.included base
+  	puts "#{base} Swimming module was included!"
+  end
+end
+
+class Duck
+  def fly; "can't fly" end
+  def swim; "can't swim" end
+  def quack; "can't quack" end
+  def eat; "eats all around" end
+end
+
+class RealDuck < Duck
+  include Quacking
+  include Swimming
+  include Flying
+end
+class RubberDuck < Duck
+  include Quacking
+  include Swimming
+end
+class WoodDuck < Duck
+   include Swimming
+  def eat; "nothing eats" end
+end
+
+ducks ||= []
+ducks << RealDuck.new
+ducks << RubberDuck.new
+ducks << WoodDuck.new
+
+ducks.each do |duck|
+  puts "#{duck.class} #{duck.swim}"
+  puts "#{duck.class} #{duck.quack}"
+  puts "#{duck.class} #{duck.fly}"
+  puts "#{duck.class} #{duck.eat}"
+
+end
